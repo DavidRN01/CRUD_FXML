be89afeeb5b492a2051090fc93b43dc2139edfa0
DavidRamosNavas <DavidRamosNavas@CS-EST-PC-13145>
2021-11-09 13:43:04 +0100

    Listar pedidos

--- src/main/java/com/mycompany/crudinterfaz/PedidosController.java
+++ src/main/java/com/mycompany/crudinterfaz/PedidosController.java
@@ -6,8 +6,22 @@
 package com.mycompany.crudinterfaz;
 
 import java.net.URL;
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import models.Pedido;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 /**
  * FXML Controller class
@@ -16,14 +30,81 @@ import javafx.fxml.Initializable;
  */
 public class PedidosController implements Initializable {
 
+    @FXML
+    private Button btnCarta;
+    @FXML
+    private Button btnCrearPedido;
+    @FXML
+    private Button btnListarHoy;
+    @FXML
+    private Button btnListarTodo;
+    @FXML
+    private Button btnRecogido;
+    @FXML
+    private Button btnBorrar;
+    @FXML
+    private TableView<Pedido> tabla;
+    @FXML
+    private TableColumn<Pedido, Long> colId;
+    @FXML
+    private TableColumn<Pedido, String> colNombre;
+    @FXML
+    private TableColumn<Pedido, Double> colPrecio;
+    @FXML
+    private TableColumn<Pedido, Date> colFecha;
+    @FXML
+    private TableColumn<Pedido, String> colEstado;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
+        ObservableList<Pedido> contenido = FXCollections.observableArrayList();
+        tabla.setItems(contenido);
         
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        colPrecio.setCellValueFactory(new PropertyValueFactory<>("precio"));
+        colFecha.setCellValueFactory(new PropertyValueFactory<>("fecha"));
+        colEstado.setCellValueFactory(new PropertyValueFactory<>("estado"));
+        
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        
+        Query q = s.createQuery("FROM Pedido", Pedido.class);
+        ArrayList<Pedido> resultado = (ArrayList<Pedido>) q.list();
+        
+        contenido.addAll(resultado);
         
     }    
+
+    @FXML
+    private void mostrarCarta(ActionEvent event) {
+    }
+
+    @FXML
+    private void crearPedido(ActionEvent event) {
+    }
+
+    @FXML
+    private void listarHoy(ActionEvent event) {
+    }
+
+    @FXML
+    private void listarTodo(ActionEvent event) {
+    }
+
+    @FXML
+    private void marcarRecogido(ActionEvent event) {
+    }
+
+    @FXML
+    private void borrarPedido(ActionEvent event) {
+    }
+
+    @FXML
+    private void seleccionElemento(MouseEvent event) {
+    }
     
 }
